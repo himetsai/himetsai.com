b711db80030d26edf8c84ddb081678c0d2fde149
boogerman919 <raytsai919@gmail.com>
2023-01-10 01:11:39 -0800

    adjusted post card content spacing

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -21,13 +21,13 @@ export default function PostCard({ post }: Props) {
     >
       <Link
         href={`/shitpost/${post.slug.current}`}
-        className="flex flex-col shrink-0 w-full h-full px-2 py-4
+        className="flex flex-col shrink-0 w-full h-full px-2 py-2 md:py-4
         group-hover:bg-[#ff7777] border-[1.5px] border-[#33272a] 
         rounded-md transition text-[#33272a] duration-200 ease-in-out
         group-hover:text-[#fffffe] group-hover:scale-105"
       >
         {/* Date */}
-        <div className="flex flex-row items-center gap-1">
+        <div className="flex flex-row items-center gap-1 pb-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
@@ -51,7 +51,10 @@ export default function PostCard({ post }: Props) {
         </div>
 
         {/* Title */}
-        <h2 className="text-2xl font-bold text-start px-4 py-4 md:py-9 mb-15">
+        <h2
+          className="text-2xl md:text-3xl font-bold text-start
+            px-4 py-2 md:py-9 mb-15"
+        >
           {post.title}
         </h2>
 

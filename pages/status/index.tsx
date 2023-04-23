c19ce8bc209f3c0fea53641cbd667d4dace695dc
boogerman919 <raytsai919@gmail.com>
2023-04-23 13:31:20 -0700

    added meta data to status page

--- pages/status/index.tsx
+++ pages/status/index.tsx
@@ -1,57 +1,56 @@
 import React from "react";
+import Head from "next/head";
 import StatusTable from "../../components/StatusTable";
 import RelInfo from "../../components/RelInfo";
 import PastIncidents from "../../components/PastIncidents";
 import { GetStaticProps } from "next";
 import { fetchIncidents } from "../../lib/fetchData/fetchIncidents";
 import { fetchStatusImage } from "../../lib/fetchData/fetchStatusImage";
-import Image from "next/image";
-import urlFor from "../../lib/urlFor";
 
 type Props = {
-  statusImage: StatusImage;
   incidents: Incident[];
 };
 
-export default function index({ statusImage, incidents }: Props) {
-  // console.log(statusImage);
+export default function index({ incidents }: Props) {
   return (
-    <div
-      id="status-container"
-      className="flex flex-col w-full h-full md:p-10 p-5 items-center"
-    >
+    <>
+      <Head>
+        <title>Status</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Status" />
+        <meta property="og:site_name" content="himetsai" />
+        <meta property="og:type" content="status page" />
+        <meta property="og:image" content="/preview.png" />
+        <meta name="description" content="Status page" key="desc" />
+      </Head>
+
       <div
-        className="flex flex-col w-full max-w-[1200px] items-center
-        bg-white sm:p-10 p-3 border-2 border-[#33272a] rounded-lg"
+        id="status-container"
+        className="flex flex-col w-full h-full md:p-10 p-5 items-center"
       >
-        {/* Title */}
-        <h1
-          className="text-2xl md:text-3xl font-bold tracking-wider 
-          md:mb-5 my-3 text-[#33272a]"
+        <div
+          className="flex flex-col w-full max-w-[1200px] items-center
+        bg-white sm:p-10 p-3 border-2 border-[#33272a] rounded-lg"
         >
-          Hime-Oodball Status
-        </h1>
+          {/* Title */}
+          <h1
+            className="text-2xl md:text-3xl font-bold tracking-wider 
+          md:mb-5 my-3 text-[#33272a]"
+          >
+            Hime-Oodball Status
+          </h1>
 
-        {/* <div className="relative shrink-0 float-right w-full aspect-[3/2] md:m-5 my-1">
-          <Image
-            className="object-cover object-center mx-auto border-[1.5px]
-          border-[#33272a] rounded-lg"
-            src={urlFor(statusImage.image).url()}
-            alt=""
-            fill
-          />
-        </div> */}
+          {/* Info */}
+          <RelInfo />
 
-        {/* Info */}
-        <RelInfo />
+          {/* Status Table */}
+          <StatusTable />
 
-        {/* Status Table */}
-        <StatusTable />
-
-        {/* Past Incidents */}
-        <PastIncidents incidents={incidents} />
+          {/* Past Incidents */}
+          <PastIncidents incidents={incidents} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 

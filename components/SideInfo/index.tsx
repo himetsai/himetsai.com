34713ba72caeadfaaca9dba5d8833312435ba585
boogerman919 <raytsai919@gmail.com>
2023-05-29 01:38:34 -0700

    fixed shitpost sideinfo position problem

--- components/SideInfo/index.tsx
+++ components/SideInfo/index.tsx
@@ -8,13 +8,15 @@ type Props = {
 };
 
 export default function SideInfo({ post }: Props) {
+  const height = window.innerHeight;
   return (
     <motion.div
       // initial={{ opacity: 0, x: -500 }}
       // animate={{ opacity: 1, x: 0 }}
       // transition={{ duration: 1 }}
-      className="fixed left-0 top-60 py-7 pl-4 pr-8 space-y-5 bg-[#fffffe] 
-    border-2 border-l-0 border-[#33272a] rounded-r-lg"
+      className={`fixed left-0 top-[50%] py-7 pl-4 pr-8 space-y-5 bg-[#fffffe] 
+    border-2 border-l-0 border-[#33272a] rounded-r-lg`}
+      style={{ transform: "translateY(-50%)" }}
     >
       {/* Publishing Date */}
       <div className="relative tracking-wide">

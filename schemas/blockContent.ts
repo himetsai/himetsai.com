9f1071da7ba81aeeebb30a926de16fd5b605891f
boogerman919 <raytsai919@gmail.com>
2023-07-16 01:16:15 +0800

    added underline and strike through decoration to text

--- schemas/blockContent.ts
+++ schemas/blockContent.ts
@@ -39,6 +39,8 @@ export default defineType({
           { title: "Strong", value: "strong" },
           { title: "Emphasis", value: "em" },
           { title: "Code", value: "code" },
+          { title: "Underline", value: "underline" },
+          { title: "Strike", value: "strike-through" },
         ],
         // Annotations can be any object structure – e.g. a link or a footnote.
         annotations: [

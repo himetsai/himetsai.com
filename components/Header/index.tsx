19dd7ad4c10c719f6b20df5b8bcb9cb48521e6d4
boogerman919 <raytsai919@gmail.com>
2023-09-09 18:33:11 +0800

    removed 聖恩mode

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -27,10 +27,10 @@ export default function Header({ position }: Props) {
         >
           {/* scroll to top if page is home */}
           {isHome ? (
-            <p className="font-bold text-2xl cursor-pointer">台北啟聰學校</p>
+            <p className="font-bold text-2xl cursor-pointer">himetsai</p>
           ) : (
             <Link href="/" className="font-bold text-2xl cursor-pointer">
-              台北啟聰學校
+              himetsai
             </Link>
           )}
         </div>

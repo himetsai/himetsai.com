f438e132f24eef158370fbc3f10cdf8edd6e6457
boogerman919 <raytsai919@gmail.com>
2024-03-28 19:51:36 -0700

    changed some layout shit

--- components/HomePage/contact/index.tsx
+++ components/HomePage/contact/index.tsx
@@ -11,7 +11,7 @@ export default function Contact({ socials }: Props) {
       className="flex flex-col relative max-w-[800px] bg-[#fffffe] rounded-xl border-2
       border-[#33272a] items-center text-left justify-evenly mx-auto p-7 sm:p-10"
     >
-      <h4 className="text-2xl font-bold tracking-wider sm:mb-7 mb-5 text-[#33272a]">
+      <h4 className="text-2xl font-bold tracking-wider sm:my-7 my-5 text-[#33272a]">
         Find Me Here
       </h4>
       <div className="grid grid-cols-4 lg:grid-cols-3 gap-4 text-[#33272a]">
